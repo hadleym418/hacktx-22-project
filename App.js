220efad0cb44db7c582b7677fe81a56fe5330c4c
hadleym418 <hadleym418@gmail.com>
2022-10-15 14:41:34 -0500

    h

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  NavigationContainer,
-} from "@react-navigation/native";
+import React from 'react';
+import './style.css';
+
+import TaskList from './basic/TaskList.js';
+import FetchTaskList from './api/FetchTaskList.js';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Everyone!</h1>
+      <TaskList />
+    </div>
+  );
+}
